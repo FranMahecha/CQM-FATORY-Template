a70debc58bff05312d444642424e62d5ab456a6f
FranciscoM <fmahecha@consorcioqutubminar.com>
2025-05-30 17:08:08 -0500

    Politicas, idioma en prueba 10%

--- assets/js/software.js
+++ assets/js/software.js
@@ -1,16 +1,41 @@
 $(document).ready(function () {
-    // Inicializa Isotope en el contenedor de las tarjetas
     var $grid = $('.image_load').isotope({
         itemSelector: '.grid-item',
         layoutMode: 'fitRows'
     });
 
-    // Filtro al hacer clic en los elementos del menú
     $('.menu-filtering li').on('click', function () {
         $('.menu-filtering li').removeClass('current_menu_item');
         $(this).addClass('current_menu_item');
 
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({ filter: filterValue });
+    });
+
+    const $image = $('#faqImage');
+
+    const images = {
+        collapseOne: 'assets/img/software/servicio (1).jpg',
+        collapseTwo: 'assets/img/software/servicio (2).jpg',
+        collapseThree: 'assets/img/software/servicio (3).jpg'
+    };
+
+        const $openSection = $('.collapse.show');
+    if ($openSection.length > 0) {
+        const openId = $openSection.attr('id');
+        const initialSrc = images[openId];
+        if (initialSrc) {
+            $image.attr('src', initialSrc);
+        }
+    }
+    
+    $('.collapse').on('show.bs.collapse', function () {
+        const id = $(this).attr('id');
+        const newSrc = images[id];
+        if (newSrc) {
+            $image.fadeOut(200, function () {
+                $image.attr('src', newSrc).fadeIn(200);
+            });
+        }
     });
 });
